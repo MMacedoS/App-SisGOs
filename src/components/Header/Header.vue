84dc93be4164c2f0150e8da972444b3552ebbe6a
Mauricio Macedo da Silva <macedo.mauriciogen@gmail.com>
2025-12-31 10:20:36 -0300

    feat: criando front api de serviços

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template lang="pt-br">
+    <header>
+        <h1>SISGOS</h1>
+    </header> 
+</template>
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+<style lang="">
+header {
+  background-color: #cecece;
+  padding: 20px;
+  text-align: center;
+  color: white;
+}
+</style>
